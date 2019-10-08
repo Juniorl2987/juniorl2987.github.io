3abbbacac1f1430928b57525a3f1c03946ef0a4d
Juniorl2987 <56267017+Juniorl2987@users.noreply.github.com>
2019-10-08 11:45:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Junior Li"
+let userName = "juniorl2987";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
